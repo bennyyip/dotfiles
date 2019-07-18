3dfcb2b4d58ce4c70e3a540c16ef0a8e87acf7bc
bennyyip <yebenmy@gmail.com>
2019-07-18 22:08:44 +0800

    powershell config

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,71 @@
+﻿$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+$env:_ZL_NO_PROMPT_COMMAND=1
+iex ($(lua53 $scriptDir\z.lua --init powershell) -join "`n")
+
+###############################################################################
+
+import-module $scriptDir\prompt.psm1
+
+function prompt {
+	gitFancyPrompt
+        _zlua --update
+}
+
+###############################################################################
+
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+Set-PSReadLineOption -MaximumHistoryCount 4000
+# history substring search
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Tab completion
+Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Set-PSReadlineKeyHandler -Key Ctrl+Backspace -Function UnixWordRubout
+
+Set-PSReadlineKeyHandler -Chord Ctrl+V -ScriptBlock {
+    $clipboard = Get-Clipboard -Raw
+    if ($clipboard -match '^\s*(http|ftp|magnet)' -or `
+        (Test-Path $clipboard.Trim()) ) {
+        $clipboard = $clipboard.Trim()
+        $clipboard = "'${clipboard}'"
+    }
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($clipboard)
+  }
+
+###############################################################################
+
+Import-Module cd-extras
+
+###############################################################################
+
+Import-Module Get-ChildItemColor
+
+###############################################################################
+
+. $scriptDir/Completions/_rg.ps1
+
+###############################################################################
+# ALIASES
+
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+
+remove-item Alias:gci -force -ErrorAction SilentlyContinue
+remove-item Alias:gp -force -ErrorAction SilentlyContinue
+function gst { git status  $args }
+function glg { git lg $args }
+function gci { git commit $args }
+function gcam { git commit -a -m $args }
+function gcan! { git commit -v -a --no-edit --amend $args }
+function gp { git push $args }
+function dsf { git diff $args }
+function grv { git remote -v $args }
---
+++ WindowsPowerShell/README.md
@@ -0,0 +1,16 @@
+# “That” fancy PowerShell prompt
+
+To use:
+
+- Install PSCore;
+- Install lua(http://luabinaries.sourceforge.net/)
+- Put the files in your profile directory;
+- Install / update modules:
+  - `posh-git`
+  - `PSReadline`
+  - `Get-ChildItemColor`
+  - `cd-extras`
+
+## LICENSE
+
+MIT
