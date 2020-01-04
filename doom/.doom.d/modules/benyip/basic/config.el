cc4b67aea6b9e21f8ccd99a1ba4376ed1fd4870d
bennyyip <yebenmy@gmail.com>
2020-01-04 22:16:51 +0800

    doom

---
+++ doom/.doom.d/modules/benyip/basic/config.el
@@ -0,0 +1,7 @@
+;;; benyip/config.el -*- lexical-binding: t; -*-
+
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Ben Yip"
+      user-mail-address "bennyye@protonmail.com")
