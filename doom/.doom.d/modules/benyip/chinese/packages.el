abb3b6f6c916d9ee279323716d2abd7d67a52d0f
BennyYip <yebenmy@protonmail.com>
2020-01-04 22:16:51 +0800

    doom

---
+++ doom/.doom.d/modules/benyip/chinese/packages.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; -*-
+;;; benyip/chinese/packages.el
+
+(package! liberime-config
+    :recipe (
+              :host github
+              :repo "merrickluo/liberime"
+              :files ("CMakeLists.txt" "Makefile" "src" "liberime-config.el")))
---
+++ doom/.doom.d/modules/benyip/orgmode/config.el
@@ -0,0 +1,5 @@
+;;; benyip/orgmode/config.el -*- lexical-binding: t; -*-
+
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
