bbac70608365ed2fddcd87e1fc37f1adb5dcecb6
BennyYip <yebenmy@protonmail.com>
2020-01-07 21:32:20 +0800

    doom: org mode keybindings

--- doom/.doom.d/modules/benyip/orgmode/config.el
+++ doom/.doom.d/modules/benyip/orgmode/config.el
@@ -1,5 +1,11 @@
 ;;; benyip/orgmode/config.el -*- lexical-binding: t; -*-
-
-
-;; If you intend to use org, it is recommended you change this!
-(setq org-directory "~/org/")
+(setq
+ ;; one archive file instead of many
+ org-directory "~/org"
+ org-archive-location
+ (concat org-directory "archive.org::* From %s")
+ org-log-done 'time
+ org-log-done-with-time t
+ org-bullets-bullet-list '("⁂")
+ org-ellipsis (if (char-displayable-p ?⬎)  " ▾ " nil)
+ org-startup-with-inline-images t)
