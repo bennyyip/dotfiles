22de24c7aee7a4f798f73ede6486e1958a8a1f72
BennyYip <yebenmy@protonmail.com>
2020-01-07 21:32:20 +0800

    doom: org mode keybindings

--- doom/.doom.d/modules/benyip/basic/config.el
+++ doom/.doom.d/modules/benyip/basic/config.el
@@ -4,4 +4,5 @@
 ;; These are used for a number of things, particularly for GPG configuration,
 ;; some email clients, file templates and snippets.
 (setq user-full-name "Ben Yip"
-      user-mail-address "bennyye@protonmail.com")
+      user-mail-address "bennyye@protonmail.com"
+      doom-localleader-key "\\")
