2767afc7e0e2a9b65bd69b5df6fbf23a3fd3b774
BennyYip <yebenmy@protonmail.com>
2020-07-24 19:48:51 +0800

    doom: unicode font

--- doom/.doom.d/modules/benyip/ui/config.el
+++ doom/.doom.d/modules/benyip/ui/config.el
@@ -10,7 +10,10 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 20))
+(setq doom-font (font-spec :family "Sarasa Mono Slab K" :size 20)
+      doom-variable-pitch-font (font-spec :family "Sarasa Mono Slab K")
+      doom-unicode-font (font-spec :family "Sarasa Mono Slab K"))
+
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
