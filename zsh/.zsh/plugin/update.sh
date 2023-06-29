3024e4c9ac1f9072ed3ed535dde234d54a62fd7e
bennyyip <yebenmy@protonmail.com>
2023-06-29 22:55:41 +0800

    zsh: update.sh

--- zsh/.zsh/plugin/update.sh
+++ zsh/.zsh/plugin/update.sh
@@ -3,3 +3,5 @@ git upgrade-submodules fast-syntax-highlighting
 wget https://github.com/zsh-users/zsh-autosuggestions/raw/master/zsh-autosuggestions.zsh -O zsh-autosuggestions.zsh
 wget https://github.com/skywind3000/z.lua/raw/master/z.lua -O z.lua
 wget https://github.com/skywind3000/z.lua/raw/master/z.lua.plugin.zsh -O z.lua.plugin.zsh
+wget https://github.com/hlissner/zsh-autopair/raw/master/autopair.zsh -O autopair.zsh
+wget https://github.com/shyiko/commacd/raw/master/commacd.sh -O commacd.sh
