25cdcac3bbe7aeab0820dd197c91c0795b3e6fbb
bennyyip <yebenmy@protonmail.com>
2023-08-07 17:26:07 +0800

    update

--- doom/modules/benyip/keybindings/config.el
+++ doom/modules/benyip/keybindings/config.el
@@ -14,11 +14,14 @@
    :leader
    :v "c" #'clipetty-kill-ring-save))
 
-
 (use-package! evil
   :config
   (setq evil-snipe-scope 'visible)
   (setq evil-want-Y-yank-to-eol 't)
+
+  (keymap-set evil-ex-search-keymap "C-v" (general-simulate-key "C-r \""))
+  (keymap-set evil-ex-completion-map "C-v" (general-simulate-key "C-r \""))
+  (keymap-set evil-normal-state-map "'" (general-simulate-key "C-w"))
 
   (when (modulep! :benyip format)
     (map!
@@ -27,9 +30,7 @@
 
   (map!
    :n "-" #'dired-jump                  ; like dirvish
-   :n "gb" #'switch-to-buffer
    :n "T" #'+workspace/new
-   :n "gs" #'delete-window              ; TODO: map avy and easymotion
 
    :n "[q" #'previous-error
    :n "]q" #'next-error
@@ -37,9 +38,15 @@
    :n [?\C-?] #'evil-ex-nohighlight
 
    (:prefix "C-w"
+    :n "RET" (general-simulate-key "C-x 4") ; open in other window
+    :n "'" #'other-window
     :n "o" #'doom/window-maximize-buffer) ; instead of doom/window-enlargen
 
-   :nv "gX" #'browse-url
+   (:prefix "g"
+    :n "?" #'describe-mode              ; TODO: map avy and easymotion
+    :n "s" #'delete-window              ; TODO: map avy and easymotion
+    :n "b" #'switch-to-buffer
+    :nv "X" #'browse-url)
 
    :v "." (kbd ":norm . RET")
    :v "Q" (kbd ":norm @q RET")
@@ -79,10 +86,48 @@
 
    (:prefix "f"
     :n "f" #'+default/find-file-under-here
-    :n "`" #'benyip/find-file-from-home))
+    :n "`" #'benyip/find-file-from-home)))
 
-  (define-key evil-ex-search-keymap (kbd "C-v") (general-simulate-key "C-r \""))
-  (define-key evil-ex-completion-map (kbd "C-v") (general-simulate-key "C-r \""))
 
-  (define-key evil-normal-state-map (kbd "'") (general-simulate-key "C-w"))
-  (global-set-key [remap list-buffers] 'ibuffer))
+;; HACK workaround for AltGr on Windows
+(defconst +w32-altgr-map-list
+  '(("ä" "C-M-a")
+    ("í" "C-M-b")
+    ("ç" "C-M-c")
+    ("đ" "C-M-d")
+    ("ë" "C-M-e")
+    ("è" "C-M-f")
+    ("é" "C-M-g")
+    ("ù" "C-M-h")
+    ("ï" "C-M-i")
+    ("ú" "C-M-j")
+    ("ĳ" "C-M-k")
+    ("ø" "C-M-l")
+    ("α" "C-M-m")
+    ("ñ" "C-M-n")
+    ("ö" "C-M-o")
+    ("œ" "C-M-p")
+    ("æ" "C-M-q")
+    ("ý" "C-M-r")
+    ("ß" "C-M-s")
+    ("þ" "C-M-t")
+    ("ü" "C-M-u")
+    ("ì" "C-M-v")
+    ("å" "C-M-w")
+    ("á" "C-M-x")
+    ("ÿ" "C-M-y")
+    ("à" "C-M-z")))
+
+(defun +w32-map-altgr-keys ()
+  (interactive)
+  (mapc
+   (lambda (x) (apply #'keymap-set key-translation-map x))
+   +w32-altgr-map-list))
+
+(defun +w32-unmap-altgr-keys ()
+  (interactive)
+  (mapc
+   (lambda (x) (keymap-unset key-translation-map (car x)))
+   +w32-altgr-map-list))
+
+(+w32-map-altgr-keys)
