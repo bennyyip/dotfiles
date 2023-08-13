a0b5a2fb5e042353823bdd102f861c11ccfb0bc2
bennyyip <yebenmy@protonmail.com>
2023-08-14 02:37:52 +0800

    update tridactylrc

---
+++ wezterm.lua
@@ -0,0 +1,52 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.color_scheme = 'GruvboxDark'
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.default_prog = { 'pwsh', '--nologo', '-Command', 'ssh arch' }
+end
+
+config.hide_tab_bar_if_only_one_tab = true
+
+-- AlwaysPrompt|NeverPrompt
+config.window_close_confirmation = 'NeverPrompt'
+
+config.font = wezterm.font 'Sarasa Term CL Nerd'
+config.font_size = 14.0
+-- config.freetype_load_target = "Light"
+-- config.freetype_render_target = "HorizontalLcd"
+
+config.window_padding = {
+    top    = "0px",
+    left   = "0px",
+    right  = "0px",
+    bottom = "0px",
+}
+
+config.disable_default_key_bindings = true
+config.keys = {
+    {mods="CTRL|SHIFT", key="F", action=wezterm.action{Search={CaseSensitiveString=""}}},
+    {mods="CTRL|SHIFT", key="C", action=wezterm.action{CopyTo="Clipboard"}},
+    {mods="CTRL|SHIFT", key="V", action=wezterm.action{PasteFrom="Clipboard"}},
+    {mods="SHIFT", key="Insert", action=wezterm.action{PasteFrom="PrimarySelection"}},
+    {mods="CTRL", key="-", action="DecreaseFontSize"},
+    {mods="CTRL", key="=", action="IncreaseFontSize"},
+    {mods="CTRL", key="0", action="ResetFontSize"},
+    {key="F11", action="ToggleFullScreen"},
+}
+
+config.initial_cols = 130
+config.initial_rows = 40
+
+return config
+
