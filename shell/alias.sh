cea308f35fad5efaf56f282e61f82ab801569b4e
bennyyip <yebenmy@protonmail.com>
2023-08-28 18:45:55 +0800

    shell: debian alias

--- shell/alias.sh
+++ shell/alias.sh
@@ -64,7 +64,6 @@ imgvim() {
   curl -F "name=@$1" https://img.vim-cn.com/
 }
 
-
 dsf() {
   # depends on diff-so-fancy
   git diff --patience --color=always $@ | diff-so-fancy | less --tab=4 -RFX
@@ -76,60 +75,14 @@ if exists ghq; then
   alias gget='ghq get --no-recursive --shallow'
 fi
 
-
 sshpath() {
   ipL=$(ip -o -4 addr | awk -F "inet |/" '!/127.0.0.1/ {print $2}' | sort -n | head -n 1)
   echo "root@${ipL}:$(realpath -e $1)"
 }
 
-
 alias cdtmp='cd `mktemp -d /tmp/benyip-XXXXXX`'
 
-# pacman aliases
-Syu() {
-  sudo pacman -Syu
-  pacman -Qtdq | ifne sudo pacman -Rcs -
-}
-
-alias Rcs="sudo pacman -Rcs"
-alias Ss="pacman -Ss"
-alias Si="pacman -Si"
-alias Qs="pacman -Qs"
-alias Qi="pacman -Qi"
-alias Qo="pacman -Qo"
-alias Ql="pacman -Ql"
-alias Fo="pacman -F"
-alias Fy="sudo pacman -Fy"
-alias Ssa="pacaur -Ssa"
-alias pain='sudo pacman -S --needed'
-alias painn='sudo pacmban -S'
-alias cower='cower --domain aur.tuna.tsinghua.edu.cn'
-
-paclist() {
-  # Source: https://bbs.archlinux.org/viewtopic.php?id=93683
-  LC_ALL=C pacman -Qei $(pacman -Qu | cut -d " " -f 1) | \
-    awk 'BEGIN {FS=":"} /^Name/{printf("\033[1;36m%s\033[1;37m", $2)} /^Description/{print $2}'
-}
-
-
-# alias Ge="G packages core/extra"
-# alias Gc="G community community"
-#
-# function Ga() { # 獲取PKGBUILD {{{2
-#   [ -z "$1" ] && echo "usage: Ga <aur package name>: get AUR package PKGBUILD" && return 1
-#   git clone aur@aur.archlinux.org:"$1".git
-#   rm -rf "$1"/.git
-# }
-#
-# function G() {
-#   [ -z "$3" ] && echo "usage: $0 <$2 package name>: get $2 package PKGBUILD" && return 1
-#   git clone https://git.archlinux.org/svntogit/$1.git/ -b packages/$3 --single-branch $3
-#   mv "$3"/trunk/* "$3"
-#   rm -rf "$3"/{repos,trunk,.git}
-# }
-
-
-shutdown () {
+shutdown() {
   echo -n 你确定要关机吗？
   read i
   if [[ $i == [Yy] ]]; then
@@ -138,3 +91,8 @@ shutdown () {
   fi
 }
 
+if exists pacman; then
+  source ~/.shell/arch-alias.sh
+elif exists apt; then
+  source ~/.shell/debian-alias.sh
+fi
---
+++ shell/debian-alias.sh
@@ -0,0 +1,4 @@
+alias Ss="apt search"
+alias Ql="dpkg-query -L"
+alias Qi="dpkg-query -s"
+alias pain="sudo apt install"
