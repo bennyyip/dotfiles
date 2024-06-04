b9a73a3d667e4cce994da025e41358f10c0b3d8c
bennyyip <yebenmy@protonmail.com>
2024-06-04 09:29:29 +0800

    update

--- mpv/scripts/RealTime.lua
+++ mpv/scripts/RealTime.lua
@@ -7,7 +7,7 @@ mp.add_periodic_timer(1, function()
     display_time = os.date("%H:%M")
     -- display_time = os.date("%H:%M:%S")
     ov.data =
-        "{\\fnLXGW WenKai Mono TC}{\\fs30}{\\an7}{\\b1}{\\1c&H9C9078}{\\2c&H000000}{\\3c&H000000}{\\4c&H000000}{\\1a&H80}{\\2a&H80}{\\3a&HD0}{\\4a&H80}" ..
+        "{\\fnConsolas}{\\fs30}{\\an7}{\\b1}{\\1c&H9C9078}{\\2c&H000000}{\\3c&H000000}{\\4c&H000000}{\\1a&H80}{\\2a&H80}{\\3a&HD0}{\\4a&H80}" ..
             display_time
     ov:update()
 end)
