d9f84a9ecc3969e870f069fb35a9664fe116159f
bennyyip <yebenmy@protonmail.com>
2024-06-05 09:07:39 +0800

    update

--- mpv/scripts/RealTime.lua
+++ mpv/scripts/RealTime.lua
@@ -7,7 +7,7 @@ mp.add_periodic_timer(1, function()
     display_time = os.date("%H:%M")
     -- display_time = os.date("%H:%M:%S")
     ov.data =
-        "{\\fnConsolas}{\\fs30}{\\an7}{\\b1}{\\1c&H9C9078}{\\2c&H000000}{\\3c&H000000}{\\4c&H000000}{\\1a&H80}{\\2a&H80}{\\3a&HD0}{\\4a&H80}" ..
+        "{\\fnKleeDigit}{\\fs20}{\\an7}{\\b1}{\\1c&H9C9078}{\\2c&H000000}{\\3c&H000000}{\\4c&H000000}{\\1a&H80}{\\2a&H80}{\\3a&HD0}{\\4a&H80}" ..
             display_time
     ov:update()
 end)
