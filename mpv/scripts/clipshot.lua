b05a2dfda5c912459e41e8b746b13c5d322c55d0
bennyyip <yebenmy@protonmail.com>
2024-08-07 11:21:15 +0800

    update

--- mpv/scripts/clipshot.lua
+++ mpv/scripts/clipshot.lua
@@ -6,6 +6,7 @@
 ---@class ClipshotOptions
 ---@field name string
 ---@field type string
+local utils = require 'mp.utils'
 local o = {
     name = 'mpv-screenshot.png',
     type = 'image/png' -- defaults to jpeg
@@ -21,8 +22,8 @@ if platform == 'windows' then
         'powershell', '-NoProfile', '-Command',
         'Add-Type -Assembly System.Windows.Forms, System.Drawing;',
         string.format(
-            "[Windows.Forms.Clipboard]::SetImage([Drawing.Image]::FromFile('%s'))",
-            file:gsub("'", "''")
+        "[Windows.Forms.Clipboard]::SetImage([Drawing.Image]::FromFile('%s'))",
+        file:gsub("'", "''")
         )
     }
 elseif platform == 'darwin' then
@@ -31,8 +32,8 @@ elseif platform == 'darwin' then
     local type = o.type ~= '' and o.type or 'JPEG picture'
     cmd = {
         'osascript', '-e', string.format(
-            'set the clipboard to (read (POSIX file %q) as %s)',
-            file, type
+        'set the clipboard to (read (POSIX file %q) as %s)',
+        file, type
         )
     }
 else
@@ -45,10 +46,38 @@ else
     end
 end
 
+local function set_clipboard(text)
+    local res = utils.subprocess({ args = {
+        'powershell', '-NoProfile', '-Command', string.format([[& {
+            Trap {
+                Write-Error -ErrorRecord $_
+                Exit 1
+            }
+            Add-Type -AssemblyName PresentationCore
+            [System.Windows.Clipboard]::SetText('%s')
+        }]], text)
+    } })
+end
+
+local function copyTime()
+    local function divmod (a, b)
+        return math.floor(a / b), a % b
+    end
+    local time_pos = mp.get_property_number("time-pos")
+    local minutes, remainder = divmod(time_pos, 60)
+    local hours, minutes = divmod(minutes, 60)
+    local seconds = math.floor(remainder)
+    -- local milliseconds = math.floor((remainder - seconds) * 1000)
+    -- local time = string.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+    local time = string.format("%02d:%02d:%02d", hours, minutes, seconds)
+    set_clipboard(time)
+end
+
 ---@param arg string
 ---@return fun()
 local function clipshot(arg)
     return function()
+        copyTime()
         mp.commandv('screenshot-to-file', file, arg)
         mp.command_native_async({'run', unpack(cmd)}, function(suc, _, err)
             mp.osd_message(suc and 'Copied screenshot to clipboard' or err, 1)
