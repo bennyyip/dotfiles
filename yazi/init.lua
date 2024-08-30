4788757bda44919119b69c0cc6b2a81283c769e6
bennyyip <yebenmy@gmail.com>
2024-08-30 20:15:36 +0800

    update yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,2 +1,2 @@
 -- ~/.config/yazi/init.lua
--- require("relative-motions"):setup({ show_numbers="relative", show_motion = true })
+require("relative-motions"):setup({ show_numbers="relative", show_motion = true })
