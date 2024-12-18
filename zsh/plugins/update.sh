23d0d406f72d12d77c2d94b7248ad30a3c2c87a7
bennyyip <yebenmy@protonmail.com>
2024-12-18 12:08:56 +0800

    update zsh plugins

--- zsh/plugins/update.sh
+++ zsh/plugins/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xue
-git upgrade-submodules fast-syntax-highlighting &
-wget https://github.com/zsh-users/zsh-autosuggestions/raw/master/zsh-autosuggestions.zsh -O zsh-autosuggestions.zsh &
-wget https://github.com/hlissner/zsh-autopair/raw/master/autopair.zsh -O autopair.zsh &
-wget https://github.com/shyiko/commacd/raw/master/commacd.sh -O $HOME/dotfiles/shell/plugins/commacd.sh &
-wait
+git upgrade-submodules fast-syntax-highlighting
+wget https://github.com/zsh-users/zsh-autosuggestions/raw/master/zsh-autosuggestions.zsh -O zsh-autosuggestions.zsh
+wget https://github.com/hlissner/zsh-autopair/raw/master/autopair.zsh -O autopair.zsh
+wget https://github.com/shyiko/commacd/raw/master/commacd.sh -O $HOME/dotfiles/shell/plugins/commacd.sh
+wget https://github.com/ohmyzsh/ohmyzsh/raw/refs/heads/master/plugins/git/git.plugin.zsh -O git.zsh
